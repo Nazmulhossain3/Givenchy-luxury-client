9def537afcd8a87b938094a4acdd7a4a611e0e60
Md Nazmul Hossain <hossainmdnazmul268@gmail.com>
2023-10-22 12:54:31 +0600

    university section is done

--- src/Pages/Home/VersitySection.jsx
+++ src/Pages/Home/VersitySection.jsx
@@ -1,7 +1,22 @@
-    
+    import versityimg from '../../assets/img/varsity-01-data.avif'
+    import versityimg2 from '../../assets/img/varsity-04-data.avif'
     const VersitySection = () => {
         return (
-            <div>
+            <div className='bg-[#CDD1DE] mt-48'>
+
+                <div className='grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 lg:p-48 gap-12'>
+                  <div className='text-center'>
+                    <p className='mb-6'>Varsity</p>
+                    <p className='mb-12'>FOR HIM</p>
+                   <img src={versityimg} alt="" />
+                  </div>
+                   
+                   <div className='text-center'>
+                   <p className='mb-6'>Varsity</p>
+                    <p className='mb-12'>FOR HIM</p>
+                   <img src={versityimg2} alt="" />
+                   </div>
+                </div>
                 
             </div>
         );
